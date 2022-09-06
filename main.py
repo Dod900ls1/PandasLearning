4ef50ef9b7a2d25e8b3ef93aa22e64031cb97e9e
yehorboiar <boyaregor1@gmail.com>
2022-09-06 21:35:08 +0100

    In this commit I learned how to query DataFrame, and how to work with CSV files though pandas library.

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import pandas as pd
 
 
-myDataSet = {
-    "Lexa": ["BMW", "Audi"],
-    "Sasha": ["Honda", "Ferari"]
-}
+def create_series():
+    myDataSet = {
+        "Lexa": ["BMW", "Audi"],
+        "Sasha": ["Honda", "Ferari"]
+    }
 
-myDataSet0 = pd.Series(myDataSet)
-print(myDataSet0)
-print(myDataSet0[0])
+    myDataSet0 = pd.Series(myDataSet)
+    print(myDataSet0)
+    print(myDataSet0[0])
 
+
+def create_dataFrame():
+    students = [{'Name': 'Alice',
+                 'Class': 'Physics',
+                 'Score': 85},
+                {'Name': 'Jack',
+                 'Class': 'Chemistry',
+                 'Score': 82},
+                {'Name': 'Helen',
+                 'Class': 'Biology',
+                 'Score': 90}]
+    df = pd.DataFrame(students, index=["school1", "school2", "school3"])
+    print(df)
+    print(df.iloc[1])
+
+
+def DataFrame_Experements():
+    df1 = pd.read_csv("AAPL.csv")
+    cols = list(df1.columns)
+    cols = [i.lower().strip() for i in cols]
+    df1.columns = cols
+    print(df1.head())
+    print("------------------------------------------------------------------------------")
+    df2 = df1["volume"] > 75555200
+    print(df1.where(df2).head())
+    print("------------------------------------------------------------------------------")
+    print(df1.where(df2).dropna().head())
+    print("------------------------------------------------------------------------------")
+    print(df1[(df1["volume"] > 70000000) & (df1["high"] > 60.4)].head())
+
+
+DataFrame_Experements()
